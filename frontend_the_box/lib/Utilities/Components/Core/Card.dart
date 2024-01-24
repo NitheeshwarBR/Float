419c478953eaf897cb4c36a2ad65e4f69b59d9a3
NITHEESHWAR B R <95460968+NitheeshwarBR@users.noreply.github.com>
2024-01-24 20:44:05 +0530

    define utilities file structure , design core card component

---
+++ frontend_the_box/lib/Utilities/Components/Core/Card.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class CardExamplesApp extends StatelessWidget {
+  final double width;
+  final double height;
+  final Color backgroundColor;
+  final double borderRadius;
+
+  const CardExamplesApp({
+    super.key,
+    required this.width,
+    required this.height,
+    required this.backgroundColor,
+    required this.borderRadius,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Center(
+        child: Card(
+          color: backgroundColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(borderRadius),
+          ),
+          child: SizedBox(
+            width: width,
+            height: height,
+            child: const Center(child: Text('Elevated Card')),
+          ),
+        ),
+      ),
+    );
+  }
+}
